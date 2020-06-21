a1e9104212319e7057b438268eeb2d68d038574e
CrazyLemon <pilicome2002@gmail.com>
2020-06-21 16:20:55 +0800

    complete scan feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'cmt-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WebScannerSample';
+  codes: { time: string; result: string }[] = [];
+  r = [];
+  allowFormats: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.CODE_39,
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+  ];
+  scanSuccess(scanResult: string) {
+    const n = new Date();
+    this.codes.push({
+      time: `${n.toLocaleDateString()} ${n.toLocaleTimeString()}`,
+      result: scanResult,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ZXingScannerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
